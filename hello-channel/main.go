6a879dc8fba27d14b5367478e92ef01d30366a5b
handuy <duy@techmaster.vn>
2020-11-26 08:48:15 +0700

    remove waitgroup

--- hello-channel/main.go
+++ hello-channel/main.go
@@ -1,43 +1,34 @@
 package main
 
 import (
-	"sync"
-
 	"github.com/go-acme/lego/v3/log"
 )
 
-func printNumber(wg *sync.WaitGroup, numChan chan int) {
+func printNumber(numChan chan int) {
 	var result int
 	for i := 0; i <= 100; i++ {
 		result += i
 	}
 	numChan <- result
-	wg.Done()
 }
 
-func printChar(wg *sync.WaitGroup, strChan chan string) {
+func printChar(strChan chan string) {
 	var result string
 	for i := 'A'; i < 'A'+26; i++ {
 		result += string(i)
 	}
 	strChan <- result
-	wg.Done()
 }
 
 func main() {
 	chanPrintNumber := make(chan int)
 	chanPrintChar := make(chan string)
 
-	var wg sync.WaitGroup
-	wg.Add(2)
-
-	go printNumber(&wg, chanPrintNumber)
-	go printChar(&wg, chanPrintChar)
+	go printNumber(chanPrintNumber)
+	go printChar(chanPrintChar)
 
 	log.Println("Kết quả từ printNumber:", <-chanPrintNumber)
 	log.Println("Kết quả từ printChar:", <-chanPrintChar)
-
-	wg.Wait()
 
 	log.Println("main finished")
 }
